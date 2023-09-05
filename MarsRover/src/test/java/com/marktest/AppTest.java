7a59ff67ee55aea83e170974f1ae4f8585f07030
NamesMark <markfirn@gmail.com>
2023-09-06 02:05:36 +0300

    Initial commit

---
+++ MarsRover/src/test/java/com/marktest/AppTest.java
@@ -0,0 +1,26 @@
+package com.marktest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void sanityCheck()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void roverCheck()
+    {
+        assertTrue( true );
+    }
+}
